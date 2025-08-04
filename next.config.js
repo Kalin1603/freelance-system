b074e41681688a14d36910096f8d1a2a92f211f0
Kalin1603 <kalintodorov03@gmail.com>
2025-08-04 22:33:41 +0300

    Convert next.config and update tsconfig

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Тук най-вероятно имаш някакви настройки, остави ги.
+  // Ако файлът е празен, просто остави празен обект {}.
+  // Например:
+  // reactStrictMode: true,
+};
+
+module.exports = nextConfig;
